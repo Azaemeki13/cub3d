b15afbe81bfb4de2029678c8b63cfd2287644597
Charles Auffret <cauffret@c1r4s5.42luxembourg.lu>
2025-08-04 15:30:30 +0200

    day1 efef

--- src/error_management.c
+++ src/error_management.c
@@ -3,54 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   error_management.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ituriel <ituriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cauffret <cauffret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 12:52:22 by chsauvag          #+#    #+#             */
-/*   Updated: 2025/08/04 14:04:49 by ituriel          ###   ########.fr       */
+/*   Updated: 2025/08/04 15:30:06 by cauffret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../include/cub3d.h"
 
 void error_msg(const char *msg)
 {
         write(2, "Error:\n", 6);
         write (2, msg, ft_strlen(msg));
         write(2, "\n", 1);
-}
-
-void	ft_free_string_array(char **arr)
-{
-        int	i;
-        if (!arr)
-                return ;
-        i = 0;
-        while (arr[i])
-        {
-                free(arr[i]);
-                arr[i] = NULL;
-                i++;
-        }
-        free(arr);
-}
-
-void ft_free_map(t_map **map)
-{
-    if (!map || !*map)
-        return;
-
-    ft_free_string_array((*map)->content);
-    free(*map);
-    *map = NULL;
-}
-
-void free_game(t_game **game)
-{
-    if (!game || !*game)
-        return;
-
-    if ((*game)->map)
-        ft_free_map(&((*game)->map));
-    free(*game);
-    *game = NULL;
 }
--- src/parser.c
+++ src/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ituriel <ituriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cauffret <cauffret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 12:49:26 by cauffret          #+#    #+#             */
-/*   Updated: 2025/08/04 14:10:10 by ituriel          ###   ########.fr       */
+/*   Updated: 2025/08/04 15:30:08 by cauffret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,34 +25,6 @@ int validate_arg (char *str)
         return (1);
 }
 
-void init_struct(t_game **game, char *path)
-{
-    *game = malloc(sizeof(*game));
-    ft_memset(*game, 0, sizeof(*game));
-    (*game)->map = malloc(sizeof(t_map));
-    init_map(game, path);
-
-}
-
-void init_map(t_game **game, char *path)
-{
-    t_map *map;
-    int fd;
-    int i;
-
-    i = 0;
-    map = (*game)->map;
-    fd = open(path, O_RDONLY);
-    if (fd < 0)
-    {
-        close(fd);
-        error_msg("Cannot open file.");
-        free_game(game);
-        exit(1);
-    }
-    while(get_next_line())
-}
-
 int validate_map(char *path, t_map **map)
 {
     int fd;
@@ -60,4 +32,5 @@ int validate_map(char *path, t_map **map)
     if (!validate_arg(path))
         return (0);
     init_map(map, path);
+    
 }
